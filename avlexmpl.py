006a507835063895263af1c92675961a2546b939
zidan_1013 <80787878+zidan4@users.noreply.github.com>
2025-03-16 20:20:43 +0300

    Update avlexmpl.py

--- avlexmpl.py
+++ avlexmpl.py
@@ -1,1 +1,12 @@
+if __name__ == "__main__":
+    avl = AVLTree()
+    root = None
+
+    # Inserting nodes
+    values = [10, 20, 30, 40, 50, 25]
+    for val in values:
+        root = avl.insert(root, val)
+
+    print("Inorder Traversal (Sorted):")
+    avl.inorder_traversal(root)  # Output: 10 20 25 30 40 50
 
